95198509c146aed427a7fb71017483f767eb8475
JDHilliard-lab <136738899+JDHilliard-lab@users.noreply.github.com>
2026-02-04 10:08:50 -0800

    Clean up navbar.js by removing a comment
    Removed unnecessary comment in navbar.js.

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const navHTML = `
     <nav class="nav-container">
         <a href="index.html" class="nav-logo">JH.</a>
-        
         <div class="nav-links">
             <a href="index.html#work">Work</a>
             <a href="mailto:jordan@jordanhilliard.com">Contact</a>
@@ -10,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
     </nav>
     `;
 
-    // This looks for a <div> with id="navbar-placeholder" and fills it
     const placeholder = document.getElementById("navbar-placeholder");
     if (placeholder) {
         placeholder.innerHTML = navHTML;
